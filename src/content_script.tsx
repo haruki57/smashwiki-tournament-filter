6b71ec90c7db69e3eb2f4023d29eecc2ffb3d612
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-04-12 22:11:02 +0900

    Cloned and do something

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -7,3 +7,22 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     sendResponse("Color message is none.");
   }
 });
+
+const tbody = document.querySelector(
+  "#mw-content-text > div.mw-parser-output > table:nth-child(112) > tbody > tr:nth-child(1)"
+)?.parentElement!;
+
+const children = Array.from(tbody.children).slice();
+tbody.replaceChildren("");
+Array.from(children)
+  .filter((element, idx) => {
+    if (idx === 0) {
+      return true;
+    }
+    return element.textContent!.indexOf("GENESIS") >= 0;
+  })
+  .forEach((element) => {
+    console.log(element.textContent);
+    tbody.appendChild(element);
+  });
+tbody.appendChild(document.createElement("div"));
